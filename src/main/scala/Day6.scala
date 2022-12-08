3f9835b74e6825ef79c61545eb52bd48c8b0bbd0
Joe Crowley <9064425-__joecrowley__@users.noreply.gitlab.com>
2022-12-08 23:06:58 +0800

    day6 complete'

--- src/main/scala/Day6.scala
+++ src/main/scala/Day6.scala
@@ -2,9 +2,10 @@ import scala.io.Source
 
 object Day6 {
 
-
   def part1(it: Iterator[Char]): Int = it.sliding(4).takeWhile(_.toSet.size < 4).length + 4
+
   def part2(it: Iterator[Char]): Int = it.sliding(14).takeWhile(_.toSet.size < 14).length + 14
+
   def main(args: Array[String]): Unit = {
     val src = Source.fromFile("input/day6/actual.txt")
     val (itp1, itp2) = src.iterator.duplicate
