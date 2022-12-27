36412a93eb1c101d11927fdd2e471083ca201915
Joe Crowley <9064425-__joecrowley__@users.noreply.gitlab.com>
2022-12-27 21:34:53 +0800

    completed day 10 part 1

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -1,3 +1,61 @@
+import java.beans.Statement
+import scala.io.Source
+
 object Day10 {
 
+
+  trait Statement {
+    def performOp: (Int, Int)
+  }
+
+  case class Addx(num: Int) extends Statement {
+    override def performOp: (Int, Int) = (num, 2)
+  }
+
+  case class Noop() extends Statement {
+    override def performOp: (Int, Int) = (0, 1)
+  }
+
+  def parseLine(line: String): Statement = {
+    val addRE = "^addx (-?\\d+)".r
+    val noopRE = "^noop".r
+    line match {
+      case addRE(num) => Addx(num.toInt)
+      case noopRE() => Noop()
+    }
+  }
+
+  case class ProgramCounter(stmt: Statement, cycle: Int, x: Long, signalStrengthSum: Long)
+
+  def runProgram(program: Vector[ProgramCounter], cyclesOfInterest: Vector[Int]): ProgramCounter = {
+    program.foldLeft(ProgramCounter(Noop(), 0, 1L, 0L)) { (acc, curr) => {
+      val cycles = acc.cycle + curr.stmt.performOp._2
+      val x = acc.x + curr.stmt.performOp._1
+      if (cyclesOfInterest.contains(cycles)) {
+        ProgramCounter(curr.stmt, cycles, x, acc.signalStrengthSum + cycles * x)
+      }
+      else if ((acc.cycle + 1 until cycles).count(cyclesOfInterest.contains(_)) != 0) {
+        val coi = (acc.cycle + 1 until cycles).filter(cyclesOfInterest.contains(_)).head
+        ProgramCounter(curr.stmt, cycles, x, acc.signalStrengthSum + acc.x * coi)
+      } else
+        ProgramCounter(curr.stmt, cycles, x, acc.signalStrengthSum)
+    }
+    }
+  }
+
+  def generateCyclesOfInterest(cyclesMax: Int): Vector[Int] =
+    (20 to cyclesMax).toVector.filter(p => (p - 20) % 40 == 0)
+
+  def part1(lines: Vector[String]): Long = {
+    val pc = lines.map(parseLine).map(ProgramCounter(_, 0, 0L, 0L))
+    val cyclesOfInterest = generateCyclesOfInterest(220)
+    runProgram(pc, cyclesOfInterest).signalStrengthSum
+  }
+
+  def main(args: Array[String]): Unit = {
+    val src = Source.fromFile("input/day10/actual.txt")
+    val lines = src.getLines().toVector
+    src.close()
+    println(s"Part1: ${part1(lines)}")
+  }
 }
--- src/test/scala/Day10Test.scala
+++ src/test/scala/Day10Test.scala
@@ -1,3 +1,95 @@
-class Day10Test {
+import org.scalatest.funspec.AnyFunSpec
+import Day10._
 
+import scala.io.Source
+class Day10Test extends AnyFunSpec {
+  describe("parseLine") {
+    it("should return Addx(5)") {
+      assert(parseLine("addx 5") == Addx(5))
+    }
+    it("should return Addx(-5)") {
+      assert(parseLine("addx -5") == Addx(-5))
+    }
+    it("should return Noop()") {
+      assert(parseLine("noop") == Noop())
+    }
+  }
+  describe("runProgram") {
+    it("should return correct x at end") {
+      val prog = Vector(
+        ProgramCounter(Addx(5), 0, 0, 0),
+        ProgramCounter(Addx(5), 0, 0, 0),
+      )
+      assert(runProgram(prog, Vector(0)).x == 11L)
+    }
+    it("should return correct x with negatives") {
+      val program = Vector(
+        ProgramCounter(Addx(5), 0, 0, 0),
+        ProgramCounter(Addx(-10), 0, 0, 0)
+      )
+      assert(runProgram(program, Vector(0)).x == -4L)
+    }
+    it("should return correct ssSum for one cycle of interest") {
+      val program = Vector(
+        ProgramCounter(Addx(5), 0, 0, 0),
+        ProgramCounter(Noop(), 0, 0, 0),
+        ProgramCounter(Addx(5), 0, 0, 0)
+      )
+      assert(runProgram(program, Vector(4)).signalStrengthSum == 24L)
+    }
+    it("should return correct ssSum for two cycles of interest") {
+      val program = Vector(
+        ProgramCounter(Addx(5), 0, 0, 0),
+        ProgramCounter(Noop(), 0, 0, 0),
+        ProgramCounter(Addx(5), 0, 0, 0),
+        ProgramCounter(Addx(5), 0, 0, 0),
+      )
+      assert(runProgram(program, Vector(3, 5)).signalStrengthSum == 73L)
+    }
+    it("should return correct ssSum for three cycles of interest") {
+      val program = Vector(
+        ProgramCounter(Addx(5), 0, 0, 0),
+        ProgramCounter(Addx(5), 0, 0, 0),
+        ProgramCounter(Addx(-5), 0, 0, 0)
+      )
+      assert(runProgram(program, Vector(2, 3, 6)).signalStrengthSum == 66L)
+    }
+  }
+  describe("cyclesOfInterest") {
+    it("should return correct cyclesOfInterest") {
+      assert(generateCyclesOfInterest(200) == Vector(20, 60, 100, 140, 180))
+    }
+  }
+  describe("part1 step by step") {
+    val src = Source.fromFile("input/day10/test.txt")
+    val lines = src.getLines().toVector
+    src.close()
+    val pc = lines.map(parseLine).map(ProgramCounter(_, 0, 0L, 0L))
+    it("should return 420 on the first coi") {
+      assert(runProgram(pc, Vector(20)).signalStrengthSum == 420L)
+    }
+    it("should return 1140 for cycle 60") {
+      assert(runProgram(pc, Vector(60)).signalStrengthSum == 1140L)
+    }
+    it("should return 1800 for cycle 100") {
+      assert(runProgram(pc, Vector(100)).signalStrengthSum == 1800L)
+    }
+    it("should return 2940 for cycle 140") {
+      assert(runProgram(pc, Vector(140)).signalStrengthSum == 2940L)
+    }
+    it("should return 2880 for cycle 180") {
+      assert(runProgram(pc, Vector(180)).signalStrengthSum == 2880L)
+    }
+    it("should return 3960 for cycle 220") {
+      assert(runProgram(pc, Vector(220)).signalStrengthSum == 3960L)
+    }
+  }
+  describe("Day10.part1") {
+    it("should return 13140 for test.txt") {
+      val src = Source.fromFile("input/day10/test.txt")
+      val lines = src.getLines().toVector
+      src.close()
+      assert(part1(lines) == 13140)
+    }
+  }
 }
