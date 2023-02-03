66aa0778528c934f8a181c4f587459c45b27c6a9
burlap101 <josephcrowley85@gmail.com>
2023-02-03 21:46:02 +0800

    Revert "wip possibly going to new install"
    This reverts commit 80ea8f554eca3ca38dc4b6136254fcadf74abb13.

--- src/main/scala/Day15.scala
+++ src/main/scala/Day15.scala
@@ -1,4 +1,3 @@
-import scala.annotation.tailrec
 import scala.io.Source
 
 object Day15 {
@@ -7,105 +6,18 @@ object Day15 {
     val loc: (Long, Long)
   }
 
-  case class BoundsVector(p1: (Long, Long), p2: (Long, Long)) {
-    def y(x: Long): Option[Long] = m() match {
-      case Some(mValue) => b() match {
-        case Some(bValue) => Some(mValue * x + bValue)
-        case None => None
-      }
-      case None => None
-    }
-    def x(y: Long): Option[Long] = m() match {
-      case Some(mValue) => b() match {
-        case Some(bValue) => (y - bValue)/mValue
-        case None => 0
-      }
-      case None => b() match {
-        case Some(_) => Some(0)
-        case None => None
-      }
-    }
-    def m(): Option[Long] = (p1._1 - p1._2) match {
-      case 0 => None
-      case _ => Some((p1._2 - p2._2) / (p1._1 - p1._2))
-    }
-    def b(): Option[Long] = m() match {
-      case Some(value) => Some(p1._2 - value * p1._1)
-      case None => None
-    }
-    def intersects(that: BoundsVector): Option[(Long, Long)] = {
-      val x = (that.b() - this.b())/(this.m()-that.m())
-      if (this.y(x) == that.y(x)) Some((this.y(x), x)) else None
-    }
-  }
-
-  case class Sensor(loc: (Long, Long), beacon: Beacon, xyMax: Long = 20L) extends Position {
+  case class Sensor(loc: (Long, Long), beacon: Beacon) extends Position {
     def distance(): Long = {
       math.abs(loc._1 - beacon.loc._1) + math.abs(loc._2 - beacon.loc._2)
-    }
-    def contains(poi: (Long, Long)): Boolean = {
-      math.abs(loc._1 - poi._1) + math.abs(loc._2 - poi._2) <= distance()
-    }
-    def isOnEdge(poi: (Long, Long)): Boolean = {
-      math.abs(loc._1 - poi._1) + math.abs(loc._2 - poi._2) == distance()
-    }
-    def nextPointAlongOutside(prev: (Long, Long)): Option[(Long, Long)] = {
-      val dist = math.abs(prev._1 - loc._1) + math.abs(prev._2 - loc._2)
-      if (dist != distance()+1) {
-        None
-      } else {
-        val diff = (prev._1 - loc._1, prev._2 - loc._2)
-        val contender = diff match {
-          case (0, l) => if (l > 0) (prev._1 + 1, prev._2 - 1) else (prev._1 - 1, prev._2 + 1)
-          case (l, 0) => if (l > 0) (prev._1 - 1, prev._2 - 1) else (prev._1 + 1, prev._2 + 1)
-          case (l1, l2) =>
-            if (l1 > 0 && l2 > 0)
-              (prev._1 + 1, prev._2 - 1)
-            else if (l1 > 0 && l2 < 0)
-              (prev._1 - 1, prev._2 - 1)
-            else if (l1 < 0 && l2 < 0)
-              (prev._1 - 1, prev._2 + 1)
-            else
-              (prev._1 + 1, prev._2 + 1)
-        }
-        contender match {
-          case (x, y) if x == loc._1 && y > loc._2 => None
-//          case (x, y) if x > xyMax && 2*loc._2 - y > 0 => Some(xyMax, 2*loc._2 - y)
-//          case (x, y) if y > xyMax && 2*loc._1 - x < xyMax => Some(2*loc._1 - x,  xyMax)
-//          case (x, y) if x < 0 && 2*loc._2 - y < xyMax => Some(0, 2*loc._2 - y)
-//          case (x, y) if y < 0 && 2*loc._1 - x > 0 => Some(2*loc._1 - x, 0)
-          case (x, y) if x > xyMax || y > xyMax || x < 0 || y < 0 => nextPointAlongOutside(contender)
-          case _ => Some(contender)
-        }
-      }
-    }
-    def firstPoint(): Option[(Long, Long)] = {
-      val top = (loc._1, loc._2 + distance() + 1)
-      if (top._1 > xyMax || top._2 > xyMax || top._1 < 0 || top._2 < 0)
-        nextPointAlongOutside(top)
-      else
-        Some(top)
-    }
-    def vectors(): List[BoundsVector] = {
-      val top = (loc._1, distance() + loc._2)
-      val right = (loc._1 + distance(), loc._2)
-      val xMaxBv = BoundsVector((xyMax,0), (xyMax, xyMax))
-      val xAxisBv = BoundsVector((0, 0), (0, xyMax))
-      val yMaxBv = BoundsVector((0, xyMax), (xyMax, xyMax))
-      val yAxisBv = BoundsVector((0,0), (xyMax, 0))
-      val bv = BoundsVector(top, right).intersects(xMaxBv) match {
-        case Some(point) => if (isOnEdge(point)) BoundsVector(top, point) else BoundsVector(top, right)
-        case None => BoundsVector(top, right)
-      }
     }
   }
 
   case class Beacon(loc: (Long, Long)) extends Position
 
-  def extractSensors(lines: List[String], xyMax: Long = 0): List[Sensor] = {
+  def extractSensors(lines: List[String]): List[Sensor] = {
     val coordsRE = "Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)".r
     lines.map {
-      case coordsRE(sx, sy, bx, by) => Sensor((sx.toLong, sy.toLong), Beacon((bx.toLong, by.toLong)), xyMax)
+      case coordsRE(sx, sy, bx, by) => Sensor((sx.toLong, sy.toLong), Beacon((bx.toLong, by.toLong)))
     }
   }
 
@@ -135,44 +47,6 @@ object Day15 {
       })
     val coverage = calcCoverageForRow(roi, withinDistance)
     coverage.size
-  }
-
-  def isBlank(poi: (Long, Long))(implicit sensors: List[Sensor]): Boolean = {
-    !sensors.map(_.contains(poi)).reduceLeft(_ || _)
-  }
-
-  @tailrec
-  def findBlank(sensor: Sensor, poi: (Long, Long))(implicit sensors: List[Sensor]): Option[(Long, Long)] = {
-    print(s"exploring Sensor:${sensors.indexOf(sensor) + 1}; poi: $poi           \r")
-    sensor.firstPoint() match {
-      case poi => None
-      case _ =>
-        if (isBlank(poi))
-          Some(poi)
-        else
-          findBlank(sensor, sensor.nextPointAlongOutside(poi).head)
-    }
-  }
-
-
-  def part2(lines: List[String], xyMax: Long): String = {
-    implicit val sensors: List[Sensor] = extractSensors(lines, xyMax)
-    val tops = sensors.flatMap(_.firstPoint()).filter(isBlank)
-    if (tops.nonEmpty)
-      s"${tops.head}"
-    else {
-      var blankPoint: Option[(Long, Long)] = None
-      val res = sensors.find(s => {
-        findBlank(s, (s.loc._1 + 1, s.loc._2 + s.distance())) match {
-          case Some(value) =>
-            blankPoint = Some(value)
-            true
-          case None => false
-        }
-      })
-      println()
-      s"$res"
-    }
   }
 
   def main(args: Array[String]): Unit = {
--- src/test/scala/Day15Test.scala
+++ src/test/scala/Day15Test.scala
@@ -42,45 +42,5 @@ class Day15Test extends AnyFunSpec {
       assert(part1(lines, 10) == 26L)
     }
   }
-  describe("part2") {
-    it("should return 56000011 for test.txt") {
-      val src = Source.fromFile("input/day15/test.txt")
-      val lines = src.getLines().toList
-      src.close()
-      assert(part2(lines, 20L) == "56000011")
-    }
-  }
-  describe("Sensor.nextPointAlongOutside") {
-    val sensor = Sensor((2,2), Beacon((2,4)))
-    it("should return next point from top position clockwise") {
-      val result = sensor.nextPointAlongOutside((2,5))
-      assert(result.contains((3, 4)))
-    }
-    it("should return next point along from between top and right") {
-      val result = sensor.nextPointAlongOutside((3,4))
-      assert(result.contains((4,3)))
-    }
-    it("should return none for point beyond outside") {
-      val result = sensor.nextPointAlongOutside((5,5))
-      assert(result.isEmpty)
-    }
-    it("should return none for point inside cloud") {
-      val result = sensor.nextPointAlongOutside((2,3))
-      assert(result.isEmpty)
-    }
-  }
-  describe("Sensor.firstPoint") {
-    it("should return top dead centre for within xyMax") {
-      val s = Sensor((5, 5), Beacon((4,4)), 50)
-      assert(s.firstPoint().contains((5, 8)))
-    }
-    it("should return next point around when going y out-of-bounds") {
-      val s = Sensor((2,2), Beacon((3,4)), 5)
-      assert(s.firstPoint().contains((3,5)))
-    }
-    it("should return top dead centre wh") {
-      val s = Sensor(())
-    }
-  }
 
 }
